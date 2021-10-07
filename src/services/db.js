093debb90ad8419f70cb4236b17705f213eef5ed
iamuday1 <iamuday1@gmail.com>
2021-10-07 10:46:09 +0530

    Add files via upload

---
+++ src/services/db.js
@@ -0,0 +1,58 @@
+export const allProducts = [
+         {
+            "id": 1,
+            "name": "Winter Jacket",
+            "category": "Men Accessories",
+            "price": 20,
+            "image": "/img/winter-jacket.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Wrist Watch",
+            "category": "Men Accessories",
+            "price": 30,
+            "image": "/img/shoes.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Wrist Band",
+            "category": "Women Accessories",
+            "price": 5,
+            "image": "/img/shoes.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Hand bag",
+            "category": "Women Accessories",
+            "price": 20,
+           "image": "/img/shoes.jpg"
+        },
+        {
+            "id": 5,
+            "name": "Shoes",
+            "category": "Children Accessories",
+            "price": 20,
+            "image": "/img/shoes.jpg"
+        },
+        {
+            "id": 6,
+            "name": "Kid's Scraf",
+            "category": "Children Accessories",
+            "price": 10,
+            "image": "/img/shoes.jpg"
+        },
+        {
+            "id": 7,
+            "name": "Soccer Ball",
+            "category": "Sports Accessories",
+            "price": 20,
+            "image": "/img/shoes.jpg"
+        },
+        {
+            "id": 8,
+            "name": "Base Ball",
+            "category": "Sports Accessories",
+            "price": 15,
+            "image": "/img/shoes.jpg"
+        }
+    ];
